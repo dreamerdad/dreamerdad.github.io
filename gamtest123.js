759cc4d75165b03316874825fe028271a7faba9f
dreamerdad <hugh@vmrcommunications.com>
2023-01-31 09:46:55 -0500

    Update gamtest123.js

--- gamtest123.js
+++ gamtest123.js
@@ -1,4 +1,5 @@
-  window.googletag = window.googletag || {cmd: []};
+/*
+window.googletag = window.googletag || {cmd: []};
   googletag.cmd.push(function() {
     googletag.defineSlot('/4580722/CatholicHerald_5', 'fluid', 'div-gpt-ad-4091100-1')
              .addService(googletag.pubads());
@@ -11,3 +12,31 @@
     googletag.pubads().setCentering(true);
     googletag.enableServices();
   });
+*/
+    window.googletag = window.googletag || {cmd: []};
+googletag.cmd.push(function() {
+  googletag.defineSlot('/4580722/CatholicHerald_1', [[300,250],[160,600],[970,250],[336,280]], 'div-gpt-ad-4206571-1')
+           .addService(googletag.pubads());
+  googletag.defineSlot('/4580722/CatholicHerald_2', ['fluid',[160,600]], 'div-gpt-ad-4206571-2')
+           .addService(googletag.pubads());
+  googletag.defineSlot('/4580722/CatholicHerald_3', ['fluid',[300,250],[336,280],[160,600],[970,250]], 'div-gpt-ad-4206571-3')
+           .addService(googletag.pubads());
+  googletag.defineSlot('/4580722/CatholicHerald_4', ['fluid',[300,250],[336,280],[160,600],[970,250]], 'div-gpt-ad-4206571-4')
+           .addService(googletag.pubads());
+  googletag.defineSlot('/4580722/CatholicHerald_5', 'fluid', 'CH001-Top')
+           .addService(googletag.pubads());
+  googletag.defineSlot('/4580722/CatholicHerald_6', 'fluid', 'CH001-Bottom')
+           .addService(googletag.pubads());
+  googletag.defineSlot('/4580722/CatholicHerald_7', 'fluid', 'CH002')
+           .addService(googletag.pubads());
+  googletag.defineSlot('/4580722/CatholicHerald_8', 'fluid', 'CH003')
+           .addService(googletag.pubads());
+    googletag.defineSlot('/4580722/CatholicHerald_9', 'fluid', 'CH004-Top')
+           .addService(googletag.pubads());
+    googletag.defineSlot('/4580722/CatholicHerald_10', 'fluid', 'CH004-Bottom')
+           .addService(googletag.pubads());
+    googletag.defineSlot('/4580722/CatholicHerald_11', 'fluid', 'CH005')
+           .addService(googletag.pubads());
+  googletag.pubads().enableSingleRequest();
+  googletag.enableServices();
+});
