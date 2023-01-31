9534dd33049ae5712aa04fca1265e45ebec49312
dreamerdad <hugh@vmrcommunications.com>
2023-01-31 09:43:03 -0500

    Create gamtest123.js

---
+++ gamtest123.js
@@ -0,0 +1,13 @@
+  window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/4580722/CatholicHerald_5', 'fluid', 'div-gpt-ad-4091100-1')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/4580722/CatholicHerald_6', 'fluid', 'div-gpt-ad-4091100-2')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/4580722/CatholicHerald_7', 'fluid', 'div-gpt-ad-4091100-3')
+             .addService(googletag.pubads());
+
+    googletag.pubads().enableSingleRequest();
+    googletag.pubads().setCentering(true);
+    googletag.enableServices();
+  });
